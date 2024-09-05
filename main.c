e55796cdbfa588c3a11859c040870220c083157e
AbdAlRahman Gad <abdobngad@gmail.com>
2024-09-06 00:48:14 +0300

    add tests, clean up and format the code
    - Format the code with clang-format
    - Add more tests.

--- main.c
+++ main.c
@@ -1,86 +1,102 @@
+#include "mem.h"
 #include <stdio.h>
 #include <sys/mman.h>
-//#include <sys/resource.h>
-//#include <unistd.h>
-#include "mem.h"
-//void *
-//mmap(void *addr, size_t len, int prot, int flags, int fd, off_t offset);
+#include <unistd.h>
 
-//sbrk or mmap
+typedef struct __test_struct {
+        int a;
+        char b;
+    } test_struct;
+
+void print_pointer_address(void *ptr) {
+    printf("address: \t%08x", (unsigned int) ptr);
+    printf("\n");
+}
+
+void print_pointer_int_value(void *ptr) {
+    int *p = (int *) ptr;
+    printf("value: \t\t%d", *p);
+    printf("\n");
+}
+
+void print_pointer_char_value(void *ptr) {
+    char *p = (char *) ptr;
+    printf("value: \t\t%c", *p);
+    printf("\n");
+}
+
+void test_for_loop() {
+    char *ptr = (char *) mem_alloc(sizeof(char));
+    for (int i = 0; i < 200; ++i) {
+        ptr = (char *) mem_alloc(sizeof(char));
+        *ptr = 'a';
+        printf("ptr:\n");
+        print_pointer_char_value(ptr);
+        print_pointer_address(ptr);
+        mem_free(ptr); /// comment for different test
+    }
+}
 int main(void) {
 
-//long page_size = sysconf(_SC_PAGE_SIZE);
-//long y =  sysconf(_SC_PAGESIZE);
-//printf(" %ui", (int)page_size);
-//printf(" %ld", y);
-////printf(" %ii", x);
+    long page_size = sysconf(_SC_PAGE_SIZE);
 
-/// TODO add more tests
+    /// TODO add more tests
 
-/// TODO format the code with clang-format
+    /// TODO clean the code
 
-/// TODO clean the code
+    /// TODO add more checks for the code
 
-/// TODO add more checks for the code
+    int ret = mem_init((int) page_size * 10, p_firstfit);
 
-         int ret = mem_init(4096, p_firstfit);
-         if (ret == -1) {
-             printf("Fail");
-             return -1;
-         }
-         int *x = (int *) mem_alloc(sizeof (int));
-         if (x == NULL) {
-    printf("mem_alloc failed\n");
-    return -1;
-}
-         *x = 10;
+    if (ret == -1) {
+        printf("Fail");
+        return -1;
+    }
+    int *x = (int *) mem_alloc(sizeof(int));
+    if (x == NULL) {
+        printf("mem_alloc failed\n");
+        return -1;
+    }
+    *x = 10;
+    printf("x:\n");
+    print_pointer_int_value(x);
+    print_pointer_address(x);
 
+    mem_free(x);
+    x = NULL;
 
-         printf("%d ", *x);
-         printf("\n%p", (void*)x);
+    printf("Free x:\n");
+    print_pointer_address(x);
 
-         mem_free(x);
-         x = NULL;
+    char *rrr = (char *) mem_alloc(sizeof(char));
+    *rrr = 'f';
+    printf("rrr:\n");
+    print_pointer_address(rrr);
+    print_pointer_char_value(rrr);
+    int *t = (int *) mem_alloc(sizeof(int));
+    if (t == NULL) {
+        printf("mem_alloc failed\n");
+        return -1;
+    }
+    *t = 30;
 
-//         printf("\n%d ", *x);
-//         printf("\n%p", (void*)x);
+    printf("t:\n");
+    print_pointer_int_value(t);
+    print_pointer_address(t);
 
-//         char *rrr = (char *) mem_alloc(sizeof (char));
-//         *rrr = 'f';
-//         printf("\n%c", *rrr);
-//         printf("\n%08x", rrr);
+    test_for_loop();
 
-         int *t = (int *) mem_alloc(sizeof (int));
-         if (t == NULL) {
-    printf("mem_alloc failed\n");
-    return -1;
-}
-         *t = 30;
+    test_struct* struct_var = (test_struct *) mem_alloc(sizeof(test_struct));
+    if (struct_var == NULL) {
+        printf("mem_alloc failed\n");
+        return -1;
+    }
+    struct_var->a = 10;
+    struct_var->b = 'a';
+    printf("x:\n");
+    print_pointer_int_value(&struct_var->a);
+    print_pointer_char_value(&struct_var->b);
+    print_pointer_address(struct_var);
 
-//         printf("\n%d ", *t);
-
-//         printf("\n%p", (void*)t);
-
-         char *ptr = (char *) mem_alloc(sizeof (char));
-
-         if (ptr == NULL) {
-    printf("mem_alloc failed\n");
-    return -1;
-}
-
-            *ptr = 'a';
-           printf("\n%c", *ptr);
-           printf("\n%p", (void*)ptr);
-
-
-//           mem_free(x);
-
-
-              mem_free(t);
-           mem_free(ptr);
-           t = NULL;
-           ptr = NULL;
-
-        return 0;
-//    return mem_init(getpagesize()*2, 0);
+    return 0;
 }
--- mem.c
+++ mem.c
@@ -6,69 +6,59 @@
 #include <sys/mman.h>
 #include <unistd.h>
 
-typedef struct __node{
-        size_t size;
-        size_t number;
-        struct __node *next;
-    } node;
+typedef struct __node {
+    size_t size;
+    size_t number;
+    struct __node *next;
+} node;
 
-typedef struct __header{
-        size_t size;
-        size_t listNumber;
-    } header;
+typedef struct __header {
+    size_t size;
+    size_t listNumber;
+} header;
 
-    int POLICY;
-    node* head;
+int POLICY;
+node *head;
 
-int mem_init(int region_size, int policy){
+int mem_init(int region_size, int policy) {
+    if (region_size < 1 || policy < 1 || policy > 3) {
+        return -1;
+    }
+    POLICY = policy;
 
-        if(region_size < 1 || policy < 1 || policy > 3){
-            return -1;
-        }
-        POLICY = policy;
+    int fd = open("/dev/zero", O_RDWR);
 
-        int fd = open("/dev/zero", O_RDWR);
-
-        head = mmap(NULL, region_size, PROT_READ|PROT_WRITE, MAP_PRIVATE, fd, 0);
-        if (head == MAP_FAILED){
-            perror("mmap");
-            return -1;
-        }
-
-        head->size = region_size - sizeof(node);
-        head->number = 1;
-        head->next = NULL;
-
-        close(fd);
-        return 0;
-}
-void *mem_alloc(int size){
-    // if p_bestfit         == or =>    equal or the smallest bigger than
-    // 1 traverse all the whole list
-    if (POLICY == p_bestfit){
-        node* tempNode = head;
-        while(tempNode->next != NULL ){
-
-        }
+    head = mmap(NULL, region_size, PROT_READ | PROT_WRITE, MAP_PRIVATE, fd, 0);
+    if (head == MAP_FAILED) {
+        perror("mmap");
+        return -1;
     }
 
-    // if p_first         first
-    if (POLICY == p_firstfit){
-        node* tempNode = head;
+    head->size = region_size - sizeof(node);
+    head->number = 1;
+    head->next = NULL;
+
+    close(fd);
+    return 0;
+}
+void *mem_alloc(int size) {
+
+    if (POLICY == p_firstfit) {
+        node *tempNode = head;
         /// padding for performance
-        if (size % 4 != 0){
+        if (size % 4 != 0) {
             size = size + (4 - size % 4);
         }
 
-        uint32_t newNodeSize = size + sizeof (header);
+        uint32_t newNodeSize = size + sizeof(header);
 
-        while(tempNode){
-            if(tempNode->size >= newNodeSize){
+        while (tempNode) {
+            if (tempNode->size >= newNodeSize) {
                 /// implement the new node
 
-                char *pointer = ((char*)tempNode + sizeof(node));
-                header *header1 = (header *)pointer;
-                *header1 = (header) {
+                char *pointer = ((char *) tempNode + sizeof(node));
+                header *header1 = (header *) pointer;
+                *header1 = (header){
                         .size = size,
                         .listNumber = tempNode->number,
                 };
@@ -85,61 +75,45 @@ void *mem_alloc(int size){
 
                     tempNode->next = newNode;
                     tempNode->size = 0;
-                }else {
+                } else {
                     tempNode->size = 0;
                 }
                 return pointer;
             }
             tempNode = tempNode->next;
-            if (tempNode == NULL){
+            if (tempNode == NULL) {
                 return NULL;
             }
         }
     }
 
-    // if p_worstfit         the biggest
-    // 1 traverse all the whole list
-    if (POLICY == p_worstfit){
-        node* tempNode = head;
-        while(tempNode->next != NULL ){
-
-        }
-    }
-
     return NULL;
 }
-int mem_free(void *ptr){
+int mem_free(void *ptr) {
     if (ptr == NULL)
         return -1;
 
-    /// 1 Free the memory block pointed by ptr
-
     char *pointer = ptr;
     pointer -= sizeof(header);
-    header *header1 = (header *)pointer;
+    header *header1 = (header *) pointer;
     int size_ = header1->size;
 
-    /// 2 increase the free space size from the free space header
+    /// increase the free space size from the free space header
+    node *tempNode = head;
 
-    /// we need a number to specify the free block
-    node* tempNode = head;
-
-    while(tempNode){
-        if(tempNode->number == header1->listNumber){
+    while (tempNode) {
+        if (tempNode->number == header1->listNumber) {
             tempNode->size += size_ + sizeof(header);
             break;
         }
         tempNode = tempNode->next;
     }
 
+    /// Free the memory block pointed by ptr
     memset(pointer, 0, header1->size + sizeof(header));
     return 0;
-    /// 3 Coalesce the free space
+    /// Coalesce the free space
     /// TODO
-
-
-    return -1;
 }
-void mem_dump(){
-
+void mem_dump() {
 }
